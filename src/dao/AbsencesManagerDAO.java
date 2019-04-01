dcad96f5b5435c77d8ee127a9bdca8cc4cdcddd3
KDragon <elkarouani@gmail.com>
2019-04-01 19:56:12 +0100

    Task: creating a listener file

--- src/dao/AbsencesManagerDAO.java
+++ src/dao/AbsencesManagerDAO.java
@@ -1,14 +1,25 @@
 package dao;
 
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpSession;import entities.Absence;
 
 public class AbsencesManagerDAO {
-
+	@PersistenceContext(unitName="AbsencesManager")
 	private EntityManager em;
 	
 	public AbsencesManagerDAO (EntityManager em){
-		
+		em = (EntityManager) ((HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(true)).getAttribute("entitymanager"); 	 
+	}
+	
+	public void addAbsence(Absence absence){
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		em.persist(absence);
+		t.commit();
 	}
 	
 }
---
+++ src/listeners/ProjectListener.java
@@ -0,0 +1,104 @@
+package listeners;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionIdListener;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class ProjectListener
+ *
+ */
+@WebListener
+public class ProjectListener implements HttpSessionListener, HttpSessionAttributeListener, HttpSessionActivationListener, HttpSessionBindingListener, HttpSessionIdListener {
+
+	EntityManager em;
+	
+    /**
+     * Default constructor. 
+     */
+    public ProjectListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    	EntityManagerFactory factory = Persistence.createEntityManagerFactory("AbsencesManager");
+    	em = factory.createEntityManager();
+    	se.getSession().setAttribute("entitymanager", em);
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueBound(HttpSessionBindingEvent)
+     */
+    public void valueBound(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionIdListener#sessionIdChanged(HttpSessionEvent, String)
+     */
+    public void sessionIdChanged(HttpSessionEvent arg0, String arg1)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionDidActivate(HttpSessionEvent)
+     */
+    public void sessionDidActivate(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionWillPassivate(HttpSessionEvent)
+     */
+    public void sessionWillPassivate(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionBindingListener#valueUnbound(HttpSessionBindingEvent)
+     */
+    public void valueUnbound(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
