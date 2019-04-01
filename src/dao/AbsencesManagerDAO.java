aba19ac3bc43cb3a24816ebdd0955ee3e6724cf9
KDragon <elkarouani@gmail.com>
2019-04-01 19:46:33 +0100

    Task: adding absencesManagerDAO for absences entity

---
+++ src/dao/AbsencesManagerDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class AbsencesManagerDAO {
+
+	private EntityManager em;
+	
+	public AbsencesManagerDAO (EntityManager em){
+		
+	}
+	
+}
