be94b7793e69418e664485067a51f390f97ee6e8
KDragon <elkarouani@gmail.com>
2019-04-08 18:37:25 +0100

    Task: adding classroom entity on project

---
+++ src/entities/Salle.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="salle")
+public class Salle implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String libelle;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+}
---
+++ src/entities/Salle_.java
@@ -0,0 +1,12 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-04-08T18:35:28.631+0100")
+@StaticMetamodel(Salle.class)
+public class Salle_ {
+	public static volatile SingularAttribute<Salle, Long> id;
+	public static volatile SingularAttribute<Salle, String> libelle;
+}
